5e6d4088f0b0a8d22d0166d79869e4a5a718ac1e
Amy Brown <amybrown1130@gmail.com>
2017-02-16 13:48:32 -0600

    initial commit

---
+++ svm.py
@@ -0,0 +1,240 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 13 13:22:22 2017
+
+@author: abrown09
+"""
+
+#%% import packages
+from sklearn import datasets
+iris = datasets.load_iris()
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm
+
+#%%
+## try 1
+
+svc = svm.SVC(kernel='linear')
+X = iris.data
+y = iris.target
+svc.fit(X, y)
+
+print(svc.predict(iris.data))
+
+## plot each combos of features for each particular flower type
+
+
+#%%sepal length by sepal width
+plt.scatter(iris.data[:, 0], iris.data[:,1], c=iris.target)
+plt.xlabel(iris.feature_names[0])
+plt.ylabel(iris.feature_names[1]) # provides a clearish way of distinguishing one from the other two
+
+svc = svm.SVC(kernel='linear')
+X = iris.data[:, 0:2]
+y = iris.target
+svc.fit(X, y)
+
+from matplotlib.colors import ListedColormap
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+def plot_estimator(estimator, X, y):
+    estimator.fit(X, y)
+    x_min, x_max = X[:, 0].min() - .1, X[:, 0].max() + .1
+    y_min, y_max = X[:, 1].min() - .1, X[:, 1].max() + .1
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
+                         np.linspace(y_min, y_max, 100))
+    Z = estimator.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    # Plot also the training points
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold)
+    plt.axis('tight')
+    plt.axis('off')
+    plt.tight_layout()
+
+plot_estimator(svc, X, y)
+
+#%% sepal length by petal length
+plt.scatter(iris.data[:, 0], iris.data[:,2], c=iris.target)
+plt.xlabel(iris.feature_names[0])
+plt.ylabel(iris.feature_names[2]) # provides a clearish way of distinguishing one from the other two
+
+svc = svm.SVC(kernel='linear')
+X = iris.data[:,[0,2]]
+y = iris.target
+svc.fit(X, y)
+
+from matplotlib.colors import ListedColormap
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+def plot_estimator(estimator, X, y):
+    estimator.fit(X, y)
+    x_min, x_max = X[:, 0].min() - .1, X[:, 0].max() + .1
+    y_min, y_max = X[:, 1].min() - .1, X[:, 1].max() + .1
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
+                         np.linspace(y_min, y_max, 100))
+    Z = estimator.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    # Plot also the training points
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold)
+    plt.axis('tight')
+    plt.axis('off')
+    plt.tight_layout()
+
+plot_estimator(svc, X, y)
+
+#%% sepal length by petal width
+plt.scatter(iris.data[:, 0], iris.data[:,3], c=iris.target)
+plt.xlabel(iris.feature_names[0])
+plt.ylabel(iris.feature_names[3]) # again, may be able to distinguish one from other two
+
+svc = svm.SVC(kernel='linear')
+X = iris.data[:,[0,3]]
+y = iris.target
+svc.fit(X, y)
+
+from matplotlib.colors import ListedColormap
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+def plot_estimator(estimator, X, y):
+    estimator.fit(X, y)
+    x_min, x_max = X[:, 0].min() - .1, X[:, 0].max() + .1
+    y_min, y_max = X[:, 1].min() - .1, X[:, 1].max() + .1
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
+                         np.linspace(y_min, y_max, 100))
+    Z = estimator.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    # Plot also the training points
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold)
+    plt.axis('tight')
+    plt.axis('off')
+    plt.tight_layout()
+
+plot_estimator(svc, X, y)
+
+#%% sepal width by petal length
+plt.scatter(iris.data[:, 1], iris.data[:,2], c=iris.target)
+plt.xlabel(iris.feature_names[1])
+plt.ylabel(iris.feature_names[2]) # again, easy to distinguish blue from the other two
+
+svc = svm.SVC(kernel='linear')
+X = iris.data[:,[1,2]]
+y = iris.target
+svc.fit(X, y)
+
+from matplotlib.colors import ListedColormap
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+def plot_estimator(estimator, X, y):
+    estimator.fit(X, y)
+    x_min, x_max = X[:, 0].min() - .1, X[:, 0].max() + .1
+    y_min, y_max = X[:, 1].min() - .1, X[:, 1].max() + .1
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
+                         np.linspace(y_min, y_max, 100))
+    Z = estimator.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    # Plot also the training points
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold)
+    plt.axis('tight')
+    plt.axis('off')
+    plt.tight_layout()
+
+plot_estimator(svc, X, y)
+
+#%% sepal width by petal width
+plt.scatter(iris.data[:, 1], iris.data[:,3], c=iris.target)
+plt.xlabel(iris.feature_names[1])
+plt.ylabel(iris.feature_names[3]) # really clear way to distinguish blue from other two
+
+svc = svm.SVC(kernel='linear')
+X = iris.data[:,[1,3]]
+y = iris.target
+svc.fit(X, y)
+
+from matplotlib.colors import ListedColormap
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+def plot_estimator(estimator, X, y):
+    estimator.fit(X, y)
+    x_min, x_max = X[:, 0].min() - .1, X[:, 0].max() + .1
+    y_min, y_max = X[:, 1].min() - .1, X[:, 1].max() + .1
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
+                         np.linspace(y_min, y_max, 100))
+    Z = estimator.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    # Plot also the training points
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold)
+    plt.axis('tight')
+    plt.axis('off')
+    plt.tight_layout()
+
+plot_estimator(svc, X, y)
+
+#%% petal length by petal width
+plt.scatter(iris.data[:, 2], iris.data[:,3], c=iris.target)
+plt.xlabel(iris.feature_names[2])
+plt.ylabel(iris.feature_names[3]) # really clear way to distinguish blue from other two
+
+svc = svm.SVC(kernel='linear')
+X = iris.data[:,[2,3]]
+y = iris.target
+svc.fit(X, y)
+
+from matplotlib.colors import ListedColormap
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+def plot_estimator(estimator, X, y):
+    estimator.fit(X, y)
+    x_min, x_max = X[:, 0].min() - .1, X[:, 0].max() + .1
+    y_min, y_max = X[:, 1].min() - .1, X[:, 1].max() + .1
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
+                         np.linspace(y_min, y_max, 100))
+    Z = estimator.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    # Plot also the training points
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold)
+    plt.axis('tight')
+    plt.axis('off')
+    plt.tight_layout()
+
+plot_estimator(svc, X, y)
+
+
+""" i think blue dots = setosa, red = versacolor, and green = virginica """
+""" svc = svm.SVC(kernel='linear', C=1)""" # C parameter is used to widen the boundary margin
